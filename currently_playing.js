d6fb0ce7a39c1a2f0a8f4f6e994c325b815111c8
nichapman <56278330+nichapman@users.noreply.github.com>
2020-03-30 20:05:57 +0100

    Main App

---
+++ currently_playing.js
@@ -0,0 +1,35 @@
+const REQUEST_URL = "https://api.spotify.com/v1/me/player/currently-playing";
+const LYRICS_PAGE_URL = "http://127.0.0.1:5500/lyrics.html"
+
+var message = document.querySelector('h1');
+var albumCover = document.querySelector('img');
+var btn = document.querySelector('.btn');
+
+btn.addEventListener('click', showLyrics);
+
+function showLyrics() {
+    window.location = LYRICS_PAGE_URL;
+}
+
+var url = window.location.href;
+var token = getToken(url);
+
+function getToken(url) {
+    var hashFragment = url.substring(url.indexOf("#") + 1, url.length);
+    var parameters = hashFragment.split('&');
+    var token = parameters[0].split('=')[1];
+    return token;
+}
+
+let request = new XMLHttpRequest();
+request.open('GET', REQUEST_URL);
+request.setRequestHeader('Authorization', 'Bearer ' + token);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+    response = request.response;
+    message.textContent = "Currently Playing: " + response.item.name + " by " + response.item.artists[0].name;
+    albumCover.src = response.item.album.images[0].url;
+}
+
---
+++ login.js
@@ -0,0 +1,17 @@
+//http://127.0.0.1:5500/
+//                      #access_token=BQAvD71iRHyPLJTudhQoae-L3daJPd277FQsG9xUoZ0xY2zQc8moxbjyvPw9hx_metCCtbuzlhdaXMyWWps86t0Q84hXiJUchpQZ06DRUV18wVTEC-zWpfPQ1KoAaVQvK6lgOGju_Ub2eHmXDB8
+//                      &token_type=Bearer
+//                      &expires_in=3600
+
+const CLIENT_ID = "f53fde576f63491494e7b85e76227687";
+const RESPONSE_TYPE = "token";
+const REDIRECT_URL = "http://127.0.0.1:5500/currently_playing.html";
+const AUTH_SCOPES = "user-read-currently-playing%20user-read-playback-state";
+const authUrl = `https://accounts.spotify.com/en/authorize?client_id=${CLIENT_ID}&response_type=${RESPONSE_TYPE}&redirect_uri=${REDIRECT_URL}&scope=${AUTH_SCOPES}`;
+
+var btn = document.querySelector('.btn');
+btn.addEventListener('click', authenticate);
+
+function authenticate() {
+    window.location = authUrl;
+}
